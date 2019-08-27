0f10a07f1c5c7c304284240ac2fde57a613cb3dd
Simon Hampel <simon@hampelgroup.com>
2019-08-28 09:36:04 +1000

    forgot afterApplicationCreated and beforeApplicationDestroyed functions

--- src/TestCase.php
+++ src/TestCase.php
@@ -183,4 +183,30 @@ abstract class TestCase extends BaseTestCase
 	{
         self::$setUpHasRun = false;
 	}
+
+    /**
+     * Register a callback to be run after the application is created.
+     *
+     * @param  callable  $callback
+     * @return void
+     */
+    public function afterApplicationCreated(callable $callback)
+    {
+        $this->afterApplicationCreatedCallbacks[] = $callback;
+
+        if ($this->setUpHasRun) {
+            call_user_func($callback);
+        }
+    }
+
+    /**
+     * Register a callback to be run before the application is destroyed.
+     *
+     * @param  callable  $callback
+     * @return void
+     */
+    protected function beforeApplicationDestroyed(callable $callback)
+    {
+        $this->beforeApplicationDestroyedCallbacks[] = $callback;
+    }
 }
