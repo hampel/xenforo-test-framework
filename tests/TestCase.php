47837e5f2fb19b0634f556adb72b66e2b29430ef
Simon Hampel <simon@hampelgroup.com>
2019-09-06 09:25:58 +1000

    getMockData helper

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,4 +13,18 @@ abstract class TestCase extends BaseTestCase
      * No trailing slash!
      */
     protected $rootDir = '../../../..';
+
+	/**
+	 * Helper function to load mock data from a file (eg json)
+	 * To use, create a "mock" folder relative to the tests folder, eg:
+	 * 'src/addons/MyVendor/MyAddon/tests/mock'
+	 *
+	 * @param $file
+	 *
+	 * @return false|string
+	 */
+	protected function getMockData($file)
+	{
+		return file_get_contents(__DIR__ . '/mock/' . $file);
+	}
 }
