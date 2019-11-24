bac5ed76dd6b02022d0da3f884cb72babd24ca45
Simon Hampel <simon@hampelgroup.com>
2019-11-24 15:05:22 +1100

    updates to swapFs and added mockFs

--- src/Concerns/InteractsWithFilesystem.php
+++ src/Concerns/InteractsWithFilesystem.php
@@ -1,17 +1,33 @@
 <?php namespace Hampel\Testing\Concerns;
 
+use Closure;
+use League\Flysystem\AdapterInterface;
 use League\Flysystem\Memory\MemoryAdapter;
-use XF\Container;
+use Mockery;
 
 trait InteractsWithFilesystem
 {
 	protected function swapFs($fs)
 	{
-		$adapters = $this->app()->config('fsAdapters');
-
-		$adapters[$fs] = function ()
+		$config = $this->app()->config();
+		$config['fsAdapters'][$fs] = function ()
 		{
 			return new MemoryAdapter();
 		};
+		$this->swap('config', $config);
+	}
+
+	protected function mockFs($fs, Closure $mock = null)
+	{
+		$args = func_get_args();
+		array_shift($args);
+
+		$config = $this->app()->config();
+		$config['fsAdapters'][$fs] = function () use ($args)
+		{
+			return Mockery::mock(AdapterInterface::class, ...array_filter($args));
+		};
+		$this->swap('config', $config);
+		return $this->app()->fs()->getFilesystem($fs)->getAdapter();
 	}
 }
