f03e6bd0c8a8b358ee69d9f04dcdceea8c9f30de
Simon Hampel <simon@hampelgroup.com>
2019-11-26 10:16:05 +1100

    added isolateAddon function to Interacts with Extension

--- src/Concerns/InteractsWithExtension.php
+++ src/Concerns/InteractsWithExtension.php
@@ -26,4 +26,54 @@ trait InteractsWithExtension
 			return new \Hampel\Testing\Extension($listeners, $classExtensions);
 		});
 	}
+
+	protected function isolateAddon($addon)
+	{
+		if (empty($addon)) return;
+
+		$classExtensions = $this->getExtensionCacheData($addon);
+		$this->app()->extension()->setClassExtensions($classExtensions);
+
+		$listeners = $this->getListenerCacheData($addon);
+		$this->app()->extension()->setListeners($listeners);
+	}
+
+	private function getExtensionCacheData($addon)
+	{
+		$extensions = $this->app()->finder('XF:ClassExtension')
+			->whereAddOnActive(['disableProcessing' => true])
+			->where('active', 1)
+			->where('addon_id', '=', $addon)
+			->order(['execute_order'])
+			->fetch();
+
+		$cache = [];
+
+		foreach ($extensions AS $extension)
+		{
+			$cache[$extension->from_class][] = $extension->to_class;
+		}
+
+		return $cache;
+	}
+
+	private function getListenerCacheData($addon)
+	{
+		$listeners = $this->app()->finder('XF:CodeEventListener')
+			->whereAddOnActive(['disableProcessing' => true])
+			->where('active', 1)
+			->where('addon_id', '=', $addon)
+			->order(['event_id', 'execute_order'])
+			->fetch();
+
+		$cache = [];
+
+		foreach ($listeners AS $listener)
+		{
+			$hint = $listener['hint'] ? $listener['hint'] : '_';
+			$cache[$listener['event_id']][$hint][] = [$listener['callback_class'], $listener['callback_method']];
+		}
+
+		return $cache;
+	}
 }
