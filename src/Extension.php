07f52213a80b80b25c4b88a09a51430d6d877dca
Simon Hampel <simon@hampelgroup.com>
2024-03-14 14:36:15 +1100

    update docblock

--- src/Extension.php
+++ src/Extension.php
@@ -4,19 +4,27 @@ use XF\Extension as BaseExtension;
 
 class Extension extends BaseExtension
 {
-	protected static $globalExtensionMap = [];
+    protected static $globalExtensionMap = [];
 
-	public function extendClass($class, $fakeBaseClass = null)
-	{
-		if (array_key_exists($class, self::$globalExtensionMap))
-		{
-			return self::$globalExtensionMap[$class];
-		}
+    /**
+     * @param $class
+     * @param $fakeBaseClass
+     * @return mixed|string
+     * @throws \Exception
+     *
+     * Maintain a global extension map so we don't try to re-extend classes for every test that gets run
+     */
+    public function extendClass($class, $fakeBaseClass = null)
+    {
+        if (array_key_exists($class, self::$globalExtensionMap))
+        {
+            return self::$globalExtensionMap[$class];
+        }
 
-		$extended = parent::extendClass($class, $fakeBaseClass);
+        $extended = parent::extendClass($class, $fakeBaseClass);
 
-		self::$globalExtensionMap[$class] = $extended;
+        self::$globalExtensionMap[$class] = $extended;
 
-		return $extended;
-	}
+        return $extended;
+    }
 }
